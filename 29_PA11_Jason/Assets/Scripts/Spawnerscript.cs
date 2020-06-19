1c370a2bdff67d302b03a32fa6045e49d0faadd2
ReJason01 <64124407+ReJason01@users.noreply.github.com>
2020-06-19 12:25:15 +0800

    Updated by Jason
    Updated PA11 with Unity

---
+++ 29_PA11_Jason/Assets/Scripts/Spawnerscript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spawnerscript : MonoBehaviour
+{
+    public GameObject SpawnObject;
+    float PositionY;
+    public static int asd;
+    [SerializeField]
+    Text txt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        txt.GetComponent<Text>();
+        InvokeRepeating("SpawnObjects", 1, 1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        txt.text = "Score:" + asd;
+
+    }
+
+    void SpawnObjects()
+    {
+        //int i = Random.Range(0, SpawnObject.Length);
+        PositionY = Random.Range(4, -4f);
+        this.transform.position = new Vector3(transform.position.x, PositionY, transform.position.z);
+        GameObject clone = Instantiate(SpawnObject, transform.position, transform.rotation);
+        Destroy(clone, 5);
+    }
+}
